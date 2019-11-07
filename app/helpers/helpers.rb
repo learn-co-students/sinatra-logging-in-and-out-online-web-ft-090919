2be1d147adcb64a8e537325ef0c3c90e86f9aff9
Alyssa Yates <bosslys@icloud.com>
2019-11-07 00:03:09 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def Helpers.current_user(session)
+    @user1 = User.create(:username => "params[:username]", :password => "params[:password]", :balance => "params[:balance]")
+  end
+
+  def Helpers.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
